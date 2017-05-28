719a72eaa80645541b1dc5b526b05215fefc78ba
KJ <kwangjaekim@gmail.com>
2017-05-28 22:17:34 +0900

    Testing Modals Portfolio

--- js/modal.js
+++ js/modal.js
@@ -1,33 +1,28 @@
-// Get the modal
+var slideIndex = 1;
+showSlides(slideIndex);
 
-var modal-eco = document.getElementById('ecobridge-portfolio');
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-var modal-animaru = document.getElementById('animaru-isu-portfolio');
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-
-var img = document.getElementsByClassName("main");
-
-img.onclick = function() {
-    modal-eco.style.display = "block";
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
     
-}
-
-// Get the span element that closes the modal
-
-var span = document.getElementsByClassName("close")[0];
-
-
-// When user clicks on span (x), close the modal
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-
-// When user clicks anywhere outside of the modal, close it
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active","");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
+
