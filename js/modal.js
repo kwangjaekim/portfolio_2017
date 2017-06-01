60e49e98c56f348c040438d7b6f3fab7c875a1c7
KJ <kwangjaekim@gmail.com>
2017-06-01 17:31:32 +0900

    General Modals for Portfolio Finished

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,192 @@
-var container = document.getElementsByClassName("slideshow-container");
+// EcoBridge Modal Slides using numbers abc
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == container) {
-    container.style.display = "none";
-  }
+var slideIndex1 = 1;
+    showDivs1(slideIndex1);
+
+function plusDivs1(a) {
+    showDivs1(slideIndex1 += a);
 }
+
+function showDivs1(a) {
+    var b;
+    var c = document.getElementsByClassName("eco-bridge-slides");
+    if (a > c.length) {slideIndex1 = 1} 
+    if (a < 1) {slideIndex1 = c.length} ;
+    for (b = 0; b < c.length; b++) {
+        c[b].style.display = "none"; 
+    }
+    c[slideIndex1-1].style.display = "block"; 
+}
+
+
+
+// Animaru Isu Modal Slides using numbers def
+
+var slideIndex2 = 1;
+    showDivs2(slideIndex2);
+
+function plusDivs2(d) {
+    showDivs2(slideIndex2 += d);
+}
+
+function showDivs2(d) {
+    var e;
+    var f = document.getElementsByClassName("animaru-isu-slides");
+    if (d > f.length) {slideIndex2 = 1} 
+    if (d < 1) {slideIndex2 = f.length} ;
+    for (e = 0; e < f.length; e++) {
+        f[e].style.display = "none"; 
+    }
+    f[slideIndex2-1].style.display = "block"; 
+}
+
+
+
+// Adjustin Cafe Modal Slides using numbers ghi
+
+var slideIndex3 = 1;
+    showDivs3(slideIndex3);
+
+function plusDivs3(g) {
+    showDivs3(slideIndex3 += g);
+}
+
+function showDivs3(g) {
+    var h;
+    var i = document.getElementsByClassName("adjustin-cafe-slides");
+    if (g > i.length) {slideIndex3 = 1} 
+    if (g < 1) {slideIndex3 = i.length} ;
+    for (h = 0; h < i.length; h++) {
+        i[h].style.display = "none"; 
+    }
+    i[slideIndex3-1].style.display = "block"; 
+}
+
+
+
+// Clear View Escapes Modal Slides using numbers jkl
+
+var slideIndex4 = 1;
+    showDivs4(slideIndex4);
+
+function plusDivs4(j) {
+    showDivs4(slideIndex4 += j);
+}
+
+function showDivs4(j) {
+    var k;
+    var l = document.getElementsByClassName("clear-view-escapes-slides");
+    if (j > l.length) {slideIndex4 = 1}
+    if (j < 1) {slideIndex4 = l.length} ;
+    for (k = 0; k < l.length; k++) {
+        l[k].style.display = "none";
+    }
+    l[slideIndex4-1].style.display = "block";
+}
+
+
+// Village House Modal Slides using numbers mno
+
+var slideIndex5 = 1;
+    showDivs5(slideIndex5);
+
+function plusDivs5(m) {
+    showDivs5(slideIndex5 += m);
+}
+
+function showDivs5(m) {
+    var n;
+    var o = document.getElementsByClassName("village-house-slides");
+    if (m > o.length) {slideIndex5 = 1}
+    if (m < 1) {slideIndex5 = o.length} ;
+    for (n = 0; n < o.length; n++) {
+        o[n].style.display = "none";
+    }
+    o[slideIndex5-1].style.display = "block";
+}
+
+// AJF Modal Slides using numbers pqr
+
+var slideIndex6 = 1;
+    showDivs6(slideIndex6);
+
+function plusDivs6(p) {
+    showDivs6(slideIndex6 += p);
+}
+
+function showDivs6(p) {
+    var q;
+    var r = document.getElementsByClassName("ajf-slides");
+    if (p > r.length) {slideIndex6 = 1}
+    if (p < 1) {slideIndex6 = r.length} ;
+    for (q = 0; q < r.length; q++) {
+        r[q].style.display = "none";
+    }
+    r[slideIndex6-1].style.display = "block";
+}
+
+// SatoMega Modal Slides using numbers stu
+
+var slideIndex7 = 1;
+    showDivs7(slideIndex7);
+
+function plusDivs7(s) {
+    showDivs7(slideIndex7 += s);
+}
+
+function showDivs7(s) {
+    var t;
+    var u = document.getElementsByClassName("sato-mega-slides");
+    if (s > u.length) {slideIndex7 = 1}
+    if (s < 1) {slideIndex7 = u.length} ;
+    for (t = 0; t < u.length; t++) {
+        u[t].style.display = "none";
+    }
+    u[slideIndex7-1].style.display = "block";
+}
+
+// Till Death Do Us Slides using numbers vwx
+
+var slideIndex8 = 1;
+    showDivs8(slideIndex8);
+
+function plusDivs8(v) {
+    showDivs8(slideIndex8 += v);
+}
+
+function showDivs8(v) {
+    var w;
+    var x = document.getElementsByClassName("till-death-do-us-slides");
+    if (v > x.length) {slideIndex8 = 1}
+    if (v < 1) {slideIndex8 = x.length} ;
+    for (w = 0; w < x.length; w++) {
+        x[w].style.display = "none";
+    }
+    x[slideIndex8-1].style.display = "block";
+}
+
+
+// Wo-Man Slides using numbers yzA
+
+var slideIndex9 = 1;
+    showDivs9(slideIndex9);
+
+function plusDivs9(y) {
+    showDivs9(slideIndex9 += y);
+}
+
+function showDivs9(y) {
+    var z;
+    var A = document.getElementsByClassName("wo-man-slides");
+    if (y > A.length) {slideIndex9 = 1}
+    if (y < 1) {slideIndex9 = A.length} ;
+    for (z = 0; z < A.length; z++) {
+        A[z].style.display = "none";
+    }
+    A[slideIndex9-1].style.display = "block";
+}
+
+
+
 
 
